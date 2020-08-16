42dc5af99acec647970307d324c18bdbbdffed86
Nikola Horbach <44953799+NicolaHorbach@users.noreply.github.com>
2020-08-16 23:44:46 +0300

    Linear toggle function for all elements

---
+++ source/toggler/linear-toggle.js
@@ -0,0 +1,19 @@
+// ------------------------
+// Toggler LINEAR function:
+// ------------------------
+
+var Navigation = document.querySelectorAll('.navigation-list');
+var Toggle = document.querySelector('.toggle');
+
+changeMenuVisibility();
+
+function changeMenuVisibility () {
+  Navigation.forEach(element => {
+    element.classList.toggle('is-opened');
+  });
+  Toggle.classList.toggle('is-toggled');
+}
+
+Toggle.addEventListener('click', function() {
+  changeMenuVisibility();
+});
