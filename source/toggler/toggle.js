7274f9d3fed0c4e6d5632e28e1c52af147cdf98c
Nikola Horbach <44953799+NicolaHorbach@users.noreply.github.com>
2020-08-16 23:45:12 +0300

    Toggler for all class elements

--- source/toggler/toggle.js
+++ source/toggler/toggle.js
@@ -1,3 +1,7 @@
+// --------------
+// Normal Toggler:
+// --------------
+
 var Navigation = document.querySelector('.navigation-list');
 var Toggle = document.querySelector('.toggle');
 
@@ -5,6 +9,26 @@ changeMenuVisibility();
 
 function changeMenuVisibility () {
   Navigation.classList.toggle('is-opened');
+  Toggle.classList.toggle('is-toggled');
+}
+
+Toggle.addEventListener('click', function() {
+  changeMenuVisibility();
+});
+
+// -------------------------------
+// Toggler for all class elements:
+// -------------------------------
+
+var Navigation = document.querySelectorAll('.navigation-list');
+var Toggle = document.querySelector('.toggle');
+
+changeMenuVisibility();
+
+function changeMenuVisibility () {
+  Navigation.forEach(function(element) {
+      element.classList.toggle('is-opened');
+    });
   Toggle.classList.toggle('is-toggled');
 }
 
